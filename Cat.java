d4b2751940202767aa208150bdb01b206b9d38c2
yliang <yliang6@smith.edu>
2024-11-13 17:58:54 -0500

    Setting up the class Cat with all the method included

---
+++ Cat.java
@@ -0,0 +1,42 @@
+public class Cat implements Contract {
+
+    private String item;
+
+    public void grab(String item){
+
+    }
+
+    public String drop(String item){
+
+    }
+
+    public void examine(String item){
+
+    }
+
+    public void use(String item){
+
+    }
+
+    public boolean walk(String direction){
+
+    }
+
+    public boolean fly(int x, int y){
+
+    }
+
+    public Number shrink(){
+
+    }
+
+    public Number grow(){
+
+    }
+    public void rest(){
+
+    }
+    public void undo();
+
+
+}
